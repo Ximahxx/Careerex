271aa1a9660ec6d407fdf9af7897508c5b41c3df
Salma Abdulsalam <111268104+Ximahxx@users.noreply.github.com>
2025-04-11 09:14:24 +0100

    Add files via upload

---
+++ WEEK 3 ASSIGNMENT/index.js
@@ -0,0 +1,37 @@
+let name = "Salma";
+let age = 23;
+let nationality = "Nigerian";
+let introduction = `Hello, my name is ${name}, I am ${age} years old and I am ${nationality}.`;
+console.log(introduction);
+
+let qoute = `Your love is better than gold, sweet like honey, warm like the sun, shining my soul`
+
+
+console.log(qoute.toUpperCase());
+console.log(qoute.toLowerCase());
+
+let word = "hello";
+let reversedWord = word.split('').reverse().join('');
+console.log("Reversed word:", reversedWord);
+
+let item1 = 50
+let item2 = 70
+let item3 = 30
+let total = item1 + item2 + item3
+console.log(`The total price of the items is ${total}`);
+
+let score1 = 54
+let score2 = 67
+let score3 = 43
+let score4 = 90
+let score5 = 78
+let average = (score1 + score2 + score3 + score4 + score5) / 5
+console.log(`The average score is ${average}`);
+
+let favFood = ['Rice', "Vegetable Soup", "Fish", "Salad"]
+console.log(favFood[0]);
+console.log(favFood[favFood.length-1]);
+favFood.push("Plantain")
+favFood.unshift('Tuwo')
+console.log(favFood)
+
